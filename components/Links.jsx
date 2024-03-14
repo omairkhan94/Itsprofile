3270a55ec8ba6768fd5ff7eaf1c75792e208f67d
omair yahya <omairyahya55@gmail.com>
2024-03-14 09:28:02 -0700

    first commit

--- components/Links.jsx
+++ components/Links.jsx
@@ -1,34 +1,34 @@
-// 'use client'
+'use client'
  
-// import { usePathname } from 'next/navigation'
-// import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
  
-// export function Links() {
-//   const pathname = usePathname()
+export function Links() {
+  const pathname = usePathname()
  
-//   return (
-//     <nav>
-//       <ul>
-//         <li>
-//           <Link className={`link ${pathname === '/' ? 'active' : ''}`} href="/">
-//             Home
-//           </Link>
-//         </li>
-//         <li>
-//           <Link
-//             className={`link ${pathname === '/about' ? 'active' : ''}`}
-//             href="/about"
-//           >
-//             About
-//           </Link>
-//         </li>
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link className={`link ${pathname === '/' ? 'active' : ''}`} href="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={`link ${pathname === '/about' ? 'active' : ''}`}
+            href="/about"
+          >
+            About
+          </Link>
+        </li>
 
-//         <li>
-//           <Link className={`form ${pathname === '/' ? 'active' : ''}`} href="/form">
-//             Form
-//           </Link>
-//         </li>
-//       </ul>
-//     </nav>
-//   )
-// }
+        <li>
+          <Link className={`form ${pathname === '/' ? 'active' : ''}`} href="/form">
+            Form
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
